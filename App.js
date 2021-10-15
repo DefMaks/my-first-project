ed2aff08b6f0517d0ee351096efeebe16a1b2e75
DefMaks <defmaks@gmail.com>
2021-10-15 13:44:59 +0100

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+// import { HeaderSet } from './appParts/header';
+
+const setName = (firtstName, secondName, thirdName) => {
+  return firtstName+" "+secondName+" "+thirdName;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ScrollView>
+        <div>
+          {setName('Joyce', 'Finunu', 'Makiese')}
+        </div>
+      </ScrollView>
+
     </View>
   );
 }
---
+++ appParts/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeaderSet = () => {
+    return `Hello World !`;
+}
+
+export default HeaderSet;
