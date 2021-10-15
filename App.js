1fa3fedec706a6426fda9731260870aa59dbdbaf
DefMaks <defmaks@gmail.com>
2021-10-15 13:49:54 +0100

    App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 // import { HeaderSet } from './appParts/header';
 
 const setName = (firtstName, secondName, thirdName) => {
-  return firtstName+" "+secondName+" "+thirdName;
+  return firtstName + " " + secondName + " " + thirdName;
 }
 
 export default function App() {
@@ -12,11 +12,10 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <ScrollView>
-        <div>
-          {setName('Joyce', 'Finunu', 'Makiese')}
-        </div>
-      </ScrollView>
+
+      <div>
+        {setName('Joyce', 'Finunu', 'Makiese')}
+      </div>
 
     </View>
   );
